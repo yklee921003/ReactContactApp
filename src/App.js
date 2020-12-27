98d38beec85933dd3891422d2fbd3df54eb9d583
Yunkyung Lee <yklee921003@yahoo.com>
2020-12-27 16:05:51 -0500

    add state components inside App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContact';
+
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: '@richardkalehoff'
+        // avatarURL: 
+      },
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: '@karen_isgrigg'
+        // avatarURL: 
+      },
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis'
+        // avatarURL: 
+      }
+    ]
+
+  }
   render() {
     return (
       <div>
         Hello World
+        <ListContacts contacts={this.state.contacts}/>
       </div>
     );
   }
---
+++ src/ListContact.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ListContacts extends Component {
+ reder(){
+     return(
+         <ol className='contact-list'>
+
+         </ol>
+     )
+ }
+
+}
+export default ListContacts
